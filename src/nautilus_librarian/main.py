1314ffd070edcc2f7d0bd57c1eea92c196b3d1cd
Jose Celano <josecelano@gmail.com>
2021-12-02 12:05:36 +0000

    [#4] Fix linting

--- src/nautilus_librarian/main.py
+++ src/nautilus_librarian/main.py
@@ -25,10 +25,10 @@ def add_subcommands():
     app.add_typer(nautilus_librarian.mods.dvc.typer.commands.app, name="dvc")
     app.add_typer(nautilus_librarian.mods.git.typer.commands.app, name="git")
     app.add_typer(nautilus_librarian.mods.gpg.typer.commands.app, name="gpg")
+    app.add_typer(nautilus_librarian.mods.libvips.typer.commands.app, name="libvips")
     app.add_typer(
-        nautilus_librarian.mods.libvips.typer.commands.app, name="libvips")
-    app.add_typer(
-        nautilus_librarian.mods.namecodes.typer.commands.app, name="namecodes")
+        nautilus_librarian.mods.namecodes.typer.commands.app, name="namecodes"
+    )
 
 
 @app.command()
