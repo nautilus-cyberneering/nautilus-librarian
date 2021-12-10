89549dc87ea602e36c2fcedb51be4a32e73a8cd7
Jose Celano <josecelano@gmail.com>
2021-12-10 14:12:44 +0000

    refactor: [#21] fix linting

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,12 +6,14 @@
 # test package name discovery based on the presence of __init__.py files."
 #
 # From: https://docs.pytest.org/en/6.2.x/goodpractices.html#tests-as-part-of-application-code
-# 
+#
 # Solution:
-# 
+#
 # Apply patch to "_pytest.pathlib.resolve_package_path" method to consider all dirs in tests/ namespace packages.
 #
-# From: https://stackoverflow.com/questions/50174130/how-do-i-pytest-a-project-using-pep-420-namespace-packages/50175552#50175552
+# From:
+# https://stackoverflow.com/questions/50174130/how-do-i-pytest-a-project-using-pep-420-namespace-packages/50175552#50175552
+
 
 import pathlib
 
@@ -22,6 +24,7 @@ resolve_pkg_path_orig = _pytest.pathlib.resolve_package_path
 # we consider all dirs in tests/ to be namespace packages
 rootdir = pathlib.Path(__file__).parent.resolve()
 namespace_pkg_dirs = [str(d) for d in rootdir.iterdir() if d.is_dir()]
+
 
 # patched method
 def resolve_package_path(path):
