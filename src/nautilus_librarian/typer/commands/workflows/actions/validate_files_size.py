ca46de8c81e71d122b920cd00e066a93a0aa143c
yeraydavidrodriguez <yeray.rodriguez@edataconsulting.de>
2022-01-03 14:17:13 +0000

    Add Libvips install script to test workflow

---
+++ src/nautilus_librarian/typer/commands/workflows/actions/validate_files_size.py
@@ -0,0 +1,35 @@
+from nautilus_librarian.mods.dvc.domain.utils import (
+    extract_list_of_media_file_changes_from_dvc_diff_output,
+)
+from nautilus_librarian.mods.libvips.domain.utils import (
+    get_image_dimensions,
+)
+from nautilus_librarian.typer.commands.workflows.actions.action_result import (
+    ActionResult,
+    ErrorMessage,
+    Message,
+    ResultCode,
+)
+
+
+def validate_files_size(dvc_diff):
+    """
+    It validates all the media file names in the dvc diff.
+    """
+    if dvc_diff == "{}":
+        return ActionResult(ResultCode.EXIT, [Message("No Gold image changes found")])
+
+    filenames = extract_list_of_media_file_changes_from_dvc_diff_output(dvc_diff)
+
+    messages = []
+
+    for filename in filenames:
+        try:
+            validate_filename(filename)
+            messages.append(Message(f"{filename} ✓"))
+        except ValueError as error:
+            return ActionResult(
+                ResultCode.ABORT, [ErrorMessage(f"{filename} ✗ {error}")]
+            )
+
+    return ActionResult(ResultCode.CONTINUE, messages)
