4e95b2d7f459f545f47037c9bd6898821778e393
yeraydavidrodriguez <yeray.rodriguez@edataconsulting.de>
2022-01-10 16:17:36 +0000

    Add image process test

---
+++ tests/test_nautilus_librarian/test_mods/test_libvips/test_domain/test_image_process.py
@@ -0,0 +1,14 @@
+import pytest
+
+from nautilus_librarian.mods.libvips.domain.utils import get_image_dimensions
+from nautilus_librarian.mods.libvips.domain.process_image import (
+    process_image
+)
+
+
+def it_should_generate_a_256_x_128_image(tmp_path_factory, libvips_fixtures_dir):
+    output_path = tmp_path_factory.mktemp("repo")
+    process_image(f"{libvips_fixtures_dir}/test_image.tif", f"{output_path}/output_image.tif", 256)
+    width, height = get_image_dimensions(f"{output_path}/output_image.tif")
+    assert width == 256
+    assert height == 128
