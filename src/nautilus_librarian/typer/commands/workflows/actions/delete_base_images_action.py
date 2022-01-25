814c99e4c72019a7046280f9210688119eabdab7
yeraydavidrodriguez <yeray.rodriguez@edataconsulting.de>
2022-01-25 16:42:06 +0000

    Fix check and remove base image if exists

--- src/nautilus_librarian/typer/commands/workflows/actions/delete_base_images_action.py
+++ src/nautilus_librarian/typer/commands/workflows/actions/delete_base_images_action.py
@@ -1,4 +1,4 @@
-from os import path
+from os import path, remove
 
 from nautilus_librarian.domain.file_locator import file_locator
 from nautilus_librarian.mods.dvc.domain.api import DvcApiWrapper
@@ -19,6 +19,12 @@ def get_base_image_absolute_path(git_repo_dir, gold_image):
     return f"{git_repo_dir}/{corresponding_base_image_relative_path}"
 
 
+def remove_base_pointer_and_file_if_exists(base_filename, dvc_api_wrapper):
+    dvc_api_wrapper.remove(f"{base_filename}.dvc")
+    if path.exists(base_filename):
+        remove(base_filename)
+
+
 def delete_base_images(dvc_diff, git_repo_dir):
     """
     It deletes previously generated base images when gold images are deleted
@@ -36,8 +42,8 @@ def delete_base_images(dvc_diff, git_repo_dir):
     for filename in filenames:
         gold_filename = Filename(filename)
         base_filename = get_base_image_absolute_path(git_repo_dir, gold_filename)
-        if path.exists(base_filename):
-            dvc_api_wrapper.remove(f"{base_filename}.dvc")
+        if path.exists(f"{base_filename}.dvc"):            
+            remove_base_pointer_and_file_if_exists(base_filename, dvc_api_wrapper)
             messages.append(
                 Message(f"✓ Base image {base_filename} successfully deleted")
             )
