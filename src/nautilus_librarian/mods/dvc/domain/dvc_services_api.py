c6b7aa4b34e7d846b55c6ce8d8d087afbbd21146
yeraydavidrodriguez <yeray.rodriguez@edataconsulting.de>
2022-02-11 15:38:37 +0000

    First implementation

---
+++ src/nautilus_librarian/mods/dvc/domain/dvc_services_api.py
@@ -0,0 +1,60 @@
+from os import path
+from typing import List
+
+import api
+from nautilus_librarian.mods.namecodes.domain.filename import Filename
+from nautilus_librarian.mods.namecodes.domain.validate_filenames import (
+    is_a_library_file,
+)
+
+
+def check_filename_is_library_gold_image(filepath):
+    if not (is_a_library_file(filepath) and Filename(filepath).is_gold_image()):
+        raise InvalidGoldImage(f"{filepath} is not a valid library Gold image")
+
+
+def check_filename_is_an_existing_gold_image(filepath):
+    if not path.isfile(filepath):
+        raise InvalidGoldImage(f"{filepath} does not exist")
+    check_filename_is_library_gold_image(filepath)
+
+
+class InvalidGoldImage(AssertionError):
+    pass
+
+
+class DvcServicesApi:
+    """
+    An API wrapping up the DVC API to simplify the interface, enforce
+    domain restrictions and conceal internals
+    """
+
+    def __init__(self, repo_path):
+        self.dvc_api = api.DvcApiWrapper(repo_path)
+
+    def diff(self, a_rev: str, b_rev: str):
+        return self.dvc_api.diff(a_rev, b_rev)
+
+    def add(self, filename: str):
+        check_filename_is_an_existing_gold_image(filename)
+        return self.dvc_api.add(filename)
+
+    def push(self, filename: str):
+        check_filename_is_an_existing_gold_image(filename)
+        return self.dvc_api.push(targets=filename)
+
+    def pull(self, filename: str, remote: str):
+        check_filename_is_library_gold_image(filename)
+        return self.dvc_api.pull(targets=filename, remote=remote)
+
+    def remove(self, filename: str):
+        check_filename_is_an_existing_gold_image(filename)
+        return self.dvc_api.remove(filename)
+
+    def move(self, from_path: str, to_path: str):
+        check_filename_is_an_existing_gold_image(from_path)
+        check_filename_is_library_gold_image(to_path)
+        return self.dvc_api.move(from_path, to_path)
+
+    def get_files_to_commit(self, base_img_relative_path: str) -> List[str]:
+        return self.dvc_api.get_files_to_commit(base_img_relative_path)
