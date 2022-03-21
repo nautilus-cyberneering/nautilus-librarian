afd38dee423c944550fd12edbdc0ee6510e4edff
Jose Celano <josecelano@gmail.com>
2022-03-21 12:17:21 +0000

    test: [#100] for RenamePath intantiation

--- tests/test_nautilus_librarian/test_mods/test_dvc/test_domain/test_diff/test_renamed_path.py
+++ tests/test_nautilus_librarian/test_mods/test_dvc/test_domain/test_diff/test_renamed_path.py
@@ -1,9 +1,9 @@
-from nautilus_librarian.mods.dvc.domain.diff.path import Path
+from nautilus_librarian.mods.dvc.domain.diff.renamed_path import RenamedPath
 
 
 def it_should_contain_the_old_and_new_relative_paths_for_a_renamed_file():
 
     assert (
-        str(Path("data/000001/32/000001-32.600.2.tif"))
+        str(RenamedPath("data/000001/32/000001-32.600.2.tif", "data/000002/32/000002-32.600.2.tif"))
         == "data/000001/32/000001-32.600.2.tif"
     )
